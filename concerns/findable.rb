7321dd9dad51d871ed3b83202917d7eb78377509
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-27 21:05:12 +0000

    Automatically backed up by Learn

--- concerns/findable.rb
+++ concerns/findable.rb
@@ -4,5 +4,12 @@ module Findable
 
   def self.find_by_name(name)
     self.all.each{|thing| return thing if thing.name == name}
+    return nil
   end
+
+  def self.find_or_create_by_name
+    return self.find_by_name unless self.find_by_name
+    return self.new(name)
+  end
+
 end
