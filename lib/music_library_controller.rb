b781ce096102c4f52a4b586d6d6e0e45fa216c06
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-27 23:00:31 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -96,7 +96,6 @@ class MusicLibraryController
     sorted_names.sort.each{|name|
       sorted_songs = Song.find_by_name(name)
     }
-
     puts "Playing #{sorted_songs[num - 1].title}, by #{sorted_songs[num - 1].artist}"
   end
 
