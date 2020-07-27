e75513d37f05613c4cb9055bf6c2578f6dc9b6ab
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-27 21:13:47 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,18 +5,18 @@ module Concerns
 
   module Findable
 
-    def self.find_by_name(name)
+    def find_by_name(name)
       self.all.each{|thing| return thing if thing.name == name}
       return nil
     end
 
-    def self.find_or_create_by_name
+    def find_or_create_by_name
       return self.find_by_name unless self.find_by_name
       return self.new(name)
     end
 
   end
-  
+
 end
 
 require_all 'lib'
