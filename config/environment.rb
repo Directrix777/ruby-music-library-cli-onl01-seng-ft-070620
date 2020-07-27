e35a4941b885b4dd2e3e0e27e271571052d01635
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-27 21:18:04 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ module Concerns
     end
 
     def find_or_create_by_name(name)
-      return self.find_by_name unless self.find_by_name
+      return self.find_by_name(name) unless self.find_by_name
       return self.create(name)
     end
 
