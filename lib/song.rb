f06f864f7ad2212b7aec9f1fa812adc5fbe2a6f4
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-27 20:46:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   attr_accessor :name, :artist
   @@all = []
 
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist = artist
   end
