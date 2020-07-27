70bcccdfbb0197a796cfa68a3ab73dc6aabe8604
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-27 21:06:49 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,21 @@ require 'bundler'
 Bundler.require
 
 module Concerns
+
+  module Findable
+
+    def self.find_by_name(name)
+      self.all.each{|thing| return thing if thing.name == name}
+      return nil
+    end
+
+    def self.find_or_create_by_name
+      return self.find_by_name unless self.find_by_name
+      return self.new(name)
+    end
+
+  end
+  
 end
 
 require_all 'lib'
