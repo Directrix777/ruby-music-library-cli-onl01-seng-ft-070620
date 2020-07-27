d98cbcbd5a49b65baedfbbebf9e2d1842acd2551
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-27 21:53:32 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -13,8 +13,4 @@ class MusicImporter
     filenames.select{|filename| filename if filename.end_with?(".mp3")}
   end
 
-  def import
-    self.files.each{|filename| Song.new_by_filename(filename)}
-  end
-
 end
