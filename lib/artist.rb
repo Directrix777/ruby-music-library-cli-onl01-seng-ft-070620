453e447ba09895ad02cde56ad125c3cb27061ecb
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-27 20:19:58 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+
+
+class Artist
+
+  attr_accessor :name
+  @@all = []
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+end
